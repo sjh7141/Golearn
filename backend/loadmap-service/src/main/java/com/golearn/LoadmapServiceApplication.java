688ecd941351b9d9ab453b04e459886d5daf224c
happy16123 <51432536+happy16123@users.noreply.github.com>
2020-10-22 18:17:00 +0900

    feat : loadmap-service 구성

---
+++ backend/loadmap-service/src/main/java/com/golearn/LoadmapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.golearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadmapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadmapServiceApplication.class, args);
+	}
+
+}
