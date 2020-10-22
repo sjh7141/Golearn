4c31b174e5a71258ff112e025db8fb8361727a5c
서지훈 <sjh7141@naver.com>
2020-10-22 17:28:00 +0900

    rebase feature/login
    - axios 추가
    - 페이지 레이아웃 구현

--- frontend/vue/src/router/index.js
+++ frontend/vue/src/router/index.js
@@ -1,28 +1,34 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import Index from "@/views/Index.vue";
+import Index from '@/views/Index.vue';
+import PlayVideo from '@/views/video/play.vue';
+import Login from '@/views/Login.vue';
 
-import PlayVideo from "@/views/video/play.vue";
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: "/",
-        name: "Index",
-        component: Index,
-    },
-    {
-        path: "/video/play",
-        name: "PlayVideo",
-        component: PlayVideo,
-    },
+	{
+		path: '/',
+		name: 'Index',
+		component: Index,
+	},
+	{
+		path: '/video/play',
+		name: 'PlayVideo',
+		component: PlayVideo,
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: Login,
+	},
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    // base: process.env.BASE_URL,
-    routes,
+	mode: 'history',
+	// base: process.env.BASE_URL,
+	routes,
 });
 
 export default router;
