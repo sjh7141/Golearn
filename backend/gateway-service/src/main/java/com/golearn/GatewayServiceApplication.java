660c69f8b36a4174426481d16db8d67733f01c20
gustn16113 <gustn16113@naver.com>
2020-10-27 17:03:31 +0900

    modify : JwtPreFilter 의존성 주입

--- backend/gateway-service/src/main/java/com/golearn/GatewayServiceApplication.java
+++ backend/gateway-service/src/main/java/com/golearn/GatewayServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.golearn.filter.JwtPreFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,6 +15,11 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+	
+	@Bean
+	public JwtPreFilter preFilter() {
+		return new JwtPreFilter();
 	}
 
 }
