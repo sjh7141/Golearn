398afbbc4614ba554339ab4c82e2644c946cee4e
gustn16113 <gustn16113@naver.com>
2020-10-27 17:01:20 +0900

    feat : JWT 확인 후 Service 사용

---
+++ backend/gateway-service/src/main/java/com/golearn/filter/JwtPreFilter.java
@@ -0,0 +1,43 @@
+package com.golearn.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class JwtPreFilter extends ZuulFilter {
+
+	private static final Logger logger = LoggerFactory.getLogger(JwtPreFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		String no = (String)SecurityContextHolder.getContext().getAuthentication().getDetails();
+		
+		if (username != null) {
+			RequestContext context = RequestContext.getCurrentContext();
+			context.addZuulRequestHeader("X-USERNAME", username);
+			context.addZuulRequestHeader("X-USERNO", no);
+		}
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
