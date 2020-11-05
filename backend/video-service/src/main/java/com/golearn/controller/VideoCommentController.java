fc2a1494b54b25e0359965750035b729b7efad40
tlagyqls7 <tlagyqls7@naver.com>
2020-11-05 14:09:40 +0900

    fix:영상 관련 여러가지 수정
    댓글 페이지 수정, 대댓글 안나오게하기, 대댓글 여러개 나오게하기, 좋아요 갯수 표시, 댓글별 멤버 추가

--- backend/video-service/src/main/java/com/golearn/controller/VideoCommentController.java
+++ backend/video-service/src/main/java/com/golearn/controller/VideoCommentController.java
@@ -25,14 +25,14 @@ public class VideoCommentController {
 
     @ApiOperation(value = "댓글 상세")
     @GetMapping("/comment/details/{vid_cmt_no}")
-    public ResponseEntity<VideoComment> getVideoComment(@PathVariable("vid_cmt_no") int vidCmtNo){
-        return new ResponseEntity(videoCommentService.getVideoComment(vidCmtNo),HttpStatus.OK);
+    public ResponseEntity<VideoComment> getVideoComment(@PathVariable("vid_cmt_no") int vidCmtNo) {
+        return new ResponseEntity(videoCommentService.getVideoComment(vidCmtNo), HttpStatus.OK);
     }
 
     @ApiOperation(value = "댓글 조회")
     @GetMapping("/comment/{vid_no}")
-    public ResponseEntity<List<VideoCommentPayload>> getVideoComments(@PathVariable("vid_no") int vidNo, @RequestParam(value = "page_no", required = false, defaultValue = "0") int pageNo) {
-        return new ResponseEntity(videoCommentService.getVideoComments(vidNo, pageNo), HttpStatus.OK);
+    public ResponseEntity<List<VideoCommentPayload>> getVideoComments(@PathVariable("vid_no") int vidNo, @RequestParam(value = "page_no", required = false, defaultValue = "1") int pageNo) {
+        return new ResponseEntity(videoCommentService.getVideoComments(vidNo, pageNo < 1 ? 0 : pageNo-1), HttpStatus.OK);
     }
 
     @ApiOperation(value = "댓글 쓰기")
@@ -62,7 +62,7 @@ public class VideoCommentController {
 
     @ApiOperation(value = "대댓글 쓰기")
     @PostMapping("/comment/{vid_no}/{vid_cmt_pno}")
-    public ResponseEntity saveVideoReply(@RequestHeader("X-USERNO") int mbrNo, @PathVariable("vid_cmt_pno") int vidCmtNo, @RequestBody VideoComment videoComment) {
+    public ResponseEntity saveVideoReply(@RequestHeader("X-USERNO") int mbrNo, @PathVariable("vid_no") int vidNo, @PathVariable("vid_cmt_pno") int vidCmtNo, @RequestBody VideoComment videoComment) {
         videoComment.setMbrNo(mbrNo);
         videoComment.setVidCmtPno(vidCmtNo);
         videoCommentService.saveVideoComment(videoComment);
@@ -71,12 +71,13 @@ public class VideoCommentController {
 
     @ApiOperation(value = "대댓글 조회")
     @GetMapping("/comment/{vid_no}/{vid_cmt_pno}")
-    public ResponseEntity<List<VideoComment>> getVideoReplies(@PathVariable("vid_cmt_pno") int vidCmtNo, @RequestParam(value = "page_no", required = false, defaultValue = "0") int pageNo) {
-        return new ResponseEntity(videoCommentService.getVideoReplies(vidCmtNo, pageNo), HttpStatus.OK);
+    public ResponseEntity<List<VideoComment>> getVideoReplies(@PathVariable("vid_no") int vidNo, @PathVariable("vid_cmt_pno") int vidCmtNo, @RequestParam(value = "page_no", required = false, defaultValue = "1") int pageNo) {
+        return new ResponseEntity(videoCommentService.getVideoReplies(vidCmtNo, pageNo < 1 ? 0 : pageNo-1), HttpStatus.OK);
     }
+
     @ApiOperation(value = "대댓글 수정")
     @PutMapping("/comment/{vid_no}/{vid_cmt_pno}/{vid_cmt_no}")
-    public ResponseEntity updateVideoReply(@RequestHeader("X-USERNO") int mbrNo, @PathVariable("vid_cmt_no") int vidCmtNo, @RequestBody VideoComment videoComment) {
+    public ResponseEntity updateVideoReply(@RequestHeader("X-USERNO") int mbrNo, @PathVariable("vid_no") int vidNo, @PathVariable("vid_cmt_pno") int vidCmtPno,@PathVariable("vid_cmt_no") int vidCmtNo, @RequestBody VideoComment videoComment) {
         videoComment.setVidCmtNo(vidCmtNo);
         videoCommentService.updateVideoComment(videoComment, mbrNo);
         return new ResponseEntity(HttpStatus.OK);
@@ -84,7 +85,7 @@ public class VideoCommentController {
 
     @ApiOperation(value = "대댓글 삭제")
     @DeleteMapping("/comment/{vid_no}/{vid_cmt_pno}/{vid_cmt_no}")
-    public ResponseEntity removeVideoReply(@RequestHeader("X-USERNO") int mbrNo, @PathVariable("vid_cmt_no") int vidCmtNo) {
+    public ResponseEntity removeVideoReply(@RequestHeader("X-USERNO") int mbrNo, @PathVariable("vid_no") int vidNo,@PathVariable("vid_cmt_pno") int vidCmtPno,@PathVariable("vid_cmt_no") int vidCmtNo) {
         videoCommentService.removeVideoComment(vidCmtNo, mbrNo);
         return new ResponseEntity(HttpStatus.OK);
     }
