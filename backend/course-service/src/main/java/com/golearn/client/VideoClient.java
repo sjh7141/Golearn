570135808927a38a2df4a55db7931d69a316bc77
happy16123 <51432536+happy16123@users.noreply.github.com>
2020-11-08 01:54:16 +0900

    modify : feign

--- backend/course-service/src/main/java/com/golearn/client/VideoClient.java
+++ backend/course-service/src/main/java/com/golearn/client/VideoClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.golearn.dto.VideoDto;
 
-@FeignClient(name = "video-service")
+@FeignClient(name = "video-service", url="k3a402.p.ssafy.io:8801/video")
 public interface VideoClient {
 	
 	@GetMapping("/{vid_no}")
