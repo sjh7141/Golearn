738fa1351331ff6e1ec3c433933b4dc3cde8911b
happy16123 <51432536+happy16123@users.noreply.github.com>
2020-11-14 05:44:31 +0900

    modify : search video hide 수정

--- backend/search-service/src/main/java/com/golearn/domain/LoadmapDto.java
+++ backend/search-service/src/main/java/com/golearn/domain/LoadmapDto.java
@@ -7,7 +7,6 @@ public class LoadmapDto {
 	private int ldmNo;
 	private int mbrNo;
 	private String mbrNickname;
-	private String mbrProfile;
 	private String ldmTitle;
 	private String ldmContent;
 	private String ldmThumbnail;
@@ -36,14 +35,6 @@ public class LoadmapDto {
 
 	public void setMbrNickname(String mbrNickname) {
 		this.mbrNickname = mbrNickname;
-	}
-
-	public String getMbrProfile() {
-		return mbrProfile;
-	}
-
-	public void setMbrProfile(String mbrProfile) {
-		this.mbrProfile = mbrProfile;
 	}
 
 	public String getLdmContent() {
