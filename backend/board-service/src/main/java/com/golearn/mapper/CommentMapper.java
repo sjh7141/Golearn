10de20caf4aee47640a2146fb47303d5acf09718
gustn16113 <gustn16113@naver.com>
2020-11-17 10:06:22 +0900

    modify : 게시판 댓글 수 가져오기

--- backend/board-service/src/main/java/com/golearn/mapper/CommentMapper.java
+++ backend/board-service/src/main/java/com/golearn/mapper/CommentMapper.java
@@ -13,6 +13,7 @@ public interface CommentMapper {
 	public int findCommentCount(@Param("brdNo") int brdNo, @Param("parentNo") int parentNo );
 	public List<CommentDto> findByCommentNo(@Param("brdNo") int brdNo, @Param("parentNo") int parentNo, @Param("startIndex") int startIndex, @Param("perPageNum") int perPageNum);
 	public CommentDto findByComment(int cmtNo);
+	public int findDetailsCount(int parentNo);
 	public int registComment(CommentDto dto);
 	public int updateComment(CommentDto dto);
 	public int deleteComment(@Param("cmtNo") int cmtNo, @Param("mbrNo") int mbrNo);
